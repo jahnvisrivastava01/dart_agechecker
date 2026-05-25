43c667ef9113109a9a8ba03639b05e6b24810a89
Jahnvi Srivastava <jahnvisrivastava0104@gmail.com>
2026-05-25 19:41:02 +0530

    first commit

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  String name = "Sania";
+  int age = 21;
+
+  print("AGE CHECKER ");
+
+  if(age >= 18) {
+    print("$name is an Adult ");
+  }
+
+  else {
+    print("$name is a Minor ");
+  }
+}
